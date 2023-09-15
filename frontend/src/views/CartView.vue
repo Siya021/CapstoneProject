7b6e016d5d55081e8f5713b40d4ab887b47fb217
Azabenathi Pupuma <pupuma.azabenathi@gmail.com>
2023-09-15 15:57:43 +0200

    cart updates

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -12,7 +12,7 @@
                         <p class="title is-6"></p>
                     </div>
                     <div class="media-right">
-                        <a href="/checkout" class="btn form-contol">Checkout</a>
+                        <a @click="checkout()" class="btn form-contol">Checkout</a>
                     </div>
                 </div>
             </div>
@@ -21,14 +21,31 @@
 </template>
 <script>
 export default {
+    data() {
+        return {
+            cart: [],
+            message: ''
+        }
+    },
     computed: {
         cart() {
             return this.$store.state.cart;
         },
     },
     mounted() {
-        this.$store.dispatch("fetchOrder", this.$route.params.orderID);
+        if(localStorage.get('cart)')){
+            this.cart = JSON.parse(localStorage.getItem('cart'));
+        } else {
+            console.log("There is nothing on cart")
+            this.message = "Sorry Please add something on cart"
+        }
     },
+    methods: {
+        checkout() {
+            localStorage.clear();
+            window.location.replace("/checkout")
+        }
+    }
 }
 </script>
 <style scoped>
--- frontend/src/views/CheckoutView.vue
+++ frontend/src/views/CheckoutView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Checkout Page</h1>
+        <h3>Thank for your visit!</h3>
     </div>
 </template>
 
